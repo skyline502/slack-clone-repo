049e4bbd9d30bd516370c2cb76888e4cf0729baf
Johnny San <johnny.san@gmail.com>
2022-12-21 10:19:23 -0800

    moved demo button below form

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { login } from '../../store/session';
-import './Login.css'
-
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { login } from "../../store/session";
+import "./Login.css";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onLogin = async (e) => {
@@ -29,44 +28,67 @@ const LoginForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
-    <div className='log-in-form-container'>
-      <img src='/static/logo.svg' className='slack-logo' style={{ height: 50 }} alt="logo"></img>
+    <div className="log-in-form-container">
+      <img
+        src="/static/logo.svg"
+        className="slack-logo"
+        style={{ height: 50 }}
+        alt="logo"
+      ></img>
       <h1>Sign in to Slack</h1>
       <h4>We suggest using the email address you use at work</h4>
-      <form onSubmit={onLogin} className='log-in-form'>
+      <form onSubmit={onLogin} className="log-in-form">
         <div>
           {errors.map((error, ind) => (
-            <div key={ind} style={{color: 'red'}}>{error}</div>
+            <div key={ind} style={{ color: "red" }}>
+              {error}
+            </div>
           ))}
         </div>
         <div>
           <input
-            name='email'
-            type='text'
-            placeholder='  name@work-email.com'
+            name="email"
+            type="text"
+            placeholder="  name@work-email.com"
             value={email}
             onChange={updateEmail}
           />
         </div>
         <div>
           <input
-            name='password'
-            type='password'
-            placeholder='  password'
+            name="password"
+            type="password"
+            placeholder="  password"
             value={password}
             onChange={updatePassword}
           />
         </div>
-        <button type='submit' className='sign-in-btn'>Sign In with Email</button>
-        <button className='demo-btn' onClick={() => dispatch(login('demo@demo.com', 'password'))}>Demo user</button>
+        <button type="submit" className="sign-in-btn">
+          Sign In with Email
+        </button>
       </form>
-      <p style={{color: 'red'}}>Don't have an account? <a className='login-sgnup' href='/sign-up' style={{textDecoration:'none'}}>Sign up</a></p>
+      <button
+        className="demo-btn"
+        onClick={() => dispatch(login("demo@demo.com", "password"))}
+      >
+        Demo user
+      </button>
+      <p style={{ color: "red" }}>
+        Don't have an account?{" "}
+        <a
+          className="login-sgnup"
+          href="/sign-up"
+          style={{ textDecoration: "none" }}
+        >
+          Sign up
+        </a>
+      </p>
       <footer>
-        <div className='contact'>
+        <div className="contact">
           Contact Us:
           <a href="https://github.com/Watts-Blake">Blake Watts</a>
           <a href="https://github.com/twincarlos">Carlos Rodriguez</a>
@@ -74,7 +96,7 @@ const LoginForm = () => {
           <a href="https://github.com/jonathancchsu">Jonathan Hsu</a>
         </div>
       </footer>
-   </div>
+    </div>
   );
 };
 
